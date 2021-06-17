ac91520bc1c4da9d5c2de726a26a7f5dbf16e357
Boris Rúra <boris.rura@gmail.com>
2021-06-17 23:18:31 +0200

    Make cache aligned allocator.

---
+++ apps/conjugate_gradient/cache_aligned_allocator.h
@@ -0,0 +1,41 @@
+#ifndef APPS_CONJUGATE_GRADIENT_CACHE_ALIGNED_ALLOCATOR
+#define APPS_CONJUGATE_GRADIENT_CACHE_ALIGNED_ALLOCATOR
+
+#include <cstddef>
+#include <new>
+#include <concepts>
+
+#include <mm_malloc.h>
+
+namespace cg {
+#ifdef __cpp_lib_hardware_interference_size
+using std::hardware_destructive_interference_size;
+#else
+constexpr size_t hardware_destructive_interference_size = 64;
+#endif
+
+template<typename ValueType>
+struct cache_aligned_allocator_t
+{
+    using value_type = ValueType;
+
+    ValueType* allocate(size_t n) {
+        return reinterpret_cast<ValueType*>(_mm_malloc(n + sizeof(ValueType), hardware_destructive_interference_size));
+    }
+
+    void deallocate(ValueType* ptr, size_t /*n*/) { _mm_free(ptr); }
+};
+
+template<typename ValueType1, std::align_val_t Alignment1, typename ValueType2, std::align_val_t Alignment2>
+constexpr bool operator==(const cache_aligned_allocator_t<ValueType1>&, const cache_aligned_allocator_t<ValueType2>&) {
+    return std::same_as<ValueType1, ValueType2>;
+}
+
+template<typename ValueType1, std::align_val_t Alignment1, typename ValueType2, std::align_val_t Alignment2>
+constexpr bool operator!=(const cache_aligned_allocator_t<ValueType1>& a,
+                          const cache_aligned_allocator_t<ValueType2>& b) {
+    return !(a == b);
+}
+} // namespace cg
+
+#endif /* APPS_CONJUGATE_GRADIENT_CACHE_ALIGNED_ALLOCATOR */
--- apps/conjugate_gradient/main.cpp
+++ apps/conjugate_gradient/main.cpp
@@ -10,10 +10,11 @@
 #include <numeric>
 #include <stdexcept>
 #include <vector>
-#include <memory>
 #include <filesystem>
 #include <span>
 #include <random>
+#include <concepts>
+#include <thread>
 
 #include <mmio/mmio.h>
 #include <mm_malloc.h>
@@ -23,28 +24,14 @@
 #include <anonymouslib_avx2.h>
 #include <fire-hpp/fire.hpp>
 
+#include "cache_aligned_allocator.h"
+
 struct file_deleter_t
 {
     void operator()(FILE* file) { fclose(file); }
 };
 
-struct mm_deleter_t
-{
-    template<typename Ty>
-    void operator()(Ty* ptr) {
-        if (ptr) _mm_free(static_cast<void*>(ptr));
-    }
-};
-
 using file_t = std::unique_ptr<FILE, file_deleter_t>;
-
-template<typename Ty>
-using mm_ptr = std::unique_ptr<Ty, mm_deleter_t>;
-
-template<typename Ty>
-auto alloc_aligned(size_t size, size_t alignment) {
-    return mm_ptr<Ty>{reinterpret_cast<Ty*>(_mm_malloc(size * sizeof(Ty), alignment))};
-}
 
 void print_mm_info(const MM_typecode& typecode) {
     fmt::print("Matrix info:\n"
@@ -64,6 +51,9 @@ struct matrix_dimensions
     size_t cols;
 };
 
+template<typename Ty>
+using cache_aligned_vector = std::vector<Ty, cg::cache_aligned_allocator_t<Ty>>;
+
 struct coo_t
 {
     std::vector<double> values;
@@ -72,17 +62,13 @@ struct coo_t
 };
 
 auto generate_random_vector(size_t required_size) {
-    // TODO: make a cache-aligned allocator.
-    // generate a random RHS
-    mm_ptr<double> x_storage{
-      reinterpret_cast<double*>(_mm_malloc(required_size * sizeof(double), ANONYMOUSLIB_X86_CACHELINE))};
-    std::span x{x_storage.get(), required_size};
+    cache_aligned_vector<double> x(required_size, 0.);
 
     std::mt19937_64 prng{std::random_device{}()};
     std::uniform_int_distribution<uint64_t> dist{};
     std::ranges::generate(x, [&] { return dist(prng) % 10; });
 
-    return x_storage;
+    return x;
 }
 
 template<typename ValueType = double>
@@ -91,12 +77,12 @@ struct csr_matrix
     // TODO: better structure layout.
     matrix_dimensions dimensions{};
     size_t num_non_zero{};
-    mm_ptr<ValueType> values;
-    mm_ptr<int> rid;
-    mm_ptr<int> cid;
+    cache_aligned_vector<ValueType> values;
+    cache_aligned_vector<int> rid;
+    cache_aligned_vector<int> cid;
     anonymouslibHandle<int, unsigned int, ValueType> A;
 
-    void inputCSR() { A.inputCSR(static_cast<int>(num_non_zero), rid.get(), cid.get(), values.get()); }
+    void inputCSR() { A.inputCSR(static_cast<int>(num_non_zero), rid.data(), cid.data(), values.data()); }
 
     static auto read_coo(FILE* file, size_t non_zero) {
         coo_t retval{.values = std::vector<double>(non_zero, 0),
@@ -136,22 +122,22 @@ struct csr_matrix
         for (auto row_id : coo.row_ids) ++csr_row_counter[row_id];
 
         // prefix scan to get the starting indices of cols in a row.
-        auto rid = alloc_aligned<int>(rows + 1, ANONYMOUSLIB_X86_CACHELINE);
-        std::exclusive_scan(csr_row_counter.begin(), csr_row_counter.end(), rid.get(), 0);
+        auto rid = cache_aligned_vector<int>(rows + 1, 0);
+        std::exclusive_scan(csr_row_counter.begin(), csr_row_counter.end(), rid.begin(), 0);
 
         // reuse the csr_row_counter as offset counter for rows.
         std::ranges::fill(csr_row_counter, 0);
 
         // transform to CSR.
-        auto values = alloc_aligned<double>(non_zero, ANONYMOUSLIB_X86_CACHELINE);
-        auto cid = alloc_aligned<int>(non_zero, ANONYMOUSLIB_X86_CACHELINE);
+        auto values = cache_aligned_vector<double>(non_zero, 0);
+        auto cid = cache_aligned_vector<int>(non_zero, 0);
         for (auto i = 0; i < non_zero; ++i) {
             const auto row = coo.row_ids[i];
-            const auto row_start = rid.get()[row];
+            const auto row_start = rid[row];
             const auto offset = row_start + csr_row_counter[row]++;
 
-            cid.get()[offset] = coo.col_ids[i];
-            values.get()[offset] = coo.values[i];
+            cid[offset] = coo.col_ids[i];
+            values[offset] = coo.values[i];
         }
 
         return csr_matrix{.dimensions{.rows = static_cast<size_t>(rows), .cols = static_cast<size_t>(cols)},
@@ -214,13 +200,13 @@ int fired_main(const std::string& matrix_path = fire::arg("-m"), bool debug = fi
     auto y_ref = std::vector<double>(matrix.dimensions.rows, 0.);
     report_timed_section("Sequential SpMV", [&] {
         for (size_t i = 0; i < matrix.dimensions.rows; ++i) {
-            const auto row_start = matrix.rid.get()[i];
-            const auto row_end = matrix.rid.get()[i + 1];
+            const auto row_start = matrix.rid[i];
+            const auto row_end = matrix.rid[i + 1];
             double sum{};
             for (auto j = row_start; j < row_end; ++j) {
-                const auto column = matrix.cid.get()[j];
-                const auto value = matrix.values.get()[j];
-                sum += x.get()[column] * value * 1.0;
+                const auto column = matrix.cid[j];
+                const auto value = matrix.values[j];
+                sum += x[column] * value * 1.0;
             }
 
             y_ref[i] = sum;
@@ -229,7 +215,7 @@ int fired_main(const std::string& matrix_path = fire::arg("-m"), bool debug = fi
 
     // careful, this shuffles x around making it unusable for calculations.
     matrix.inputCSR();
-    matrix.A.setX(x.get());
+    matrix.A.setX(x.data());
     matrix.A.setSigma(ANONYMOUSLIB_CSR5_SIGMA);
     report_timed_section("CSR5 conversion", [&] {
         matrix.A.asCSR();
@@ -237,19 +223,17 @@ int fired_main(const std::string& matrix_path = fire::arg("-m"), bool debug = fi
     });
 
     // make output buff.
-    auto Y = alloc_aligned<double>(matrix.dimensions.rows, ANONYMOUSLIB_X86_CACHELINE);
-    std::fill_n(Y.get(), matrix.dimensions.rows, 0.);
-
-    report_timed_section("CSR5 SpMV", [&] { matrix.A.spmv(1.0, Y.get()); });
+    auto Y = cache_aligned_vector<double>(matrix.dimensions.rows, 0.);
+    report_timed_section("CSR5 SpMV", [&] { matrix.A.spmv(1.0, Y.data()); });
 
     constexpr auto comparator = [](auto lhs, auto rhs) { return !(std::abs(lhs - rhs) > 0.01 * std::abs(lhs)); };
 
-    auto are_same = std::ranges::equal(std::span{Y.get(), matrix.dimensions.rows}, y_ref, comparator);
+    auto are_same = std::ranges::equal(Y, y_ref, comparator);
 
     fmt::print("SPMV correct : {}\n", are_same);
     if (!are_same && debug) {
         for (size_t i = 0; i < matrix.dimensions.rows; ++i) {
-            auto val = Y.get()[i];
+            auto val = Y[i];
             auto ref = y_ref[i];
             if (!comparator(val, ref)) fmt::print("{}: {} {}\n", i, val, ref);
         }
