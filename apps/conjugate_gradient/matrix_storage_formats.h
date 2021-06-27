790a647fdbfdeafaf79834428107718584030d21
Boris Rúra <boris.rura@gmail.com>
2021-06-28 00:05:58 +0200

    Better naming.

--- apps/conjugate_gradient/matrix_storage_formats.h
+++ apps/conjugate_gradient/matrix_storage_formats.h
@@ -40,7 +40,7 @@ namespace matrix_storage_formats {
             symmetric{symmetric} {}
 
         template<std::invocable<ValueType, uint32_t, uint32_t> Callable>
-        void iterate_values(Callable&& callable) const noexcept {
+        void iterate(Callable&& callable) const noexcept {
             for (size_t i = 0; i < values.size(); ++i) {
                 const auto row = row_indices[i];
                 const auto col = col_indices[i];
@@ -74,7 +74,7 @@ namespace matrix_storage_formats {
             // count occurences of rows.
             std::vector<uint32_t> csr_row_counter(coo.dimensions.rows + 1, 0);
 
-            coo.iterate_values([&](auto /*value*/, auto row, auto /*col*/) { ++csr_row_counter[row]; });
+            coo.iterate([&](auto /*value*/, auto row, auto /*col*/) { ++csr_row_counter[row]; });
 
             // prefix scan to get the starting indices of cols in a row.
             std::exclusive_scan(csr_row_counter.begin(), csr_row_counter.end(), csr_row_counter.begin(), 0);
@@ -86,7 +86,7 @@ namespace matrix_storage_formats {
 
             // reuse the csr_row_counter as offset counter for rows.
             std::ranges::fill(csr_row_counter, 0);
-            coo.iterate_values([&](auto value, auto row, auto col) {
+            coo.iterate([&](auto value, auto row, auto col) {
                 const auto row_start = retval.row_start_offsets[row];
                 const auto offset = row_start + csr_row_counter[row]++;
 
