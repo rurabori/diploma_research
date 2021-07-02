73b92099ace702ac3587e5e06183e081994b42fb
Boris Rúra <boris.rura@gmail.com>
2021-07-02 13:39:40 +0200

    Allocator is noexcept.

--- include/dim/memory/aligned_allocator.h
+++ include/dim/memory/aligned_allocator.h
@@ -20,21 +20,22 @@ struct cache_aligned_allocator_t
 {
     using value_type = ValueType;
 
-    ValueType* allocate(size_t n) {
+    ValueType* allocate(size_t n) noexcept {
         return reinterpret_cast<ValueType*>(_mm_malloc(n * sizeof(ValueType), hardware_destructive_interference_size));
     }
 
-    void deallocate(ValueType* ptr, size_t /*n*/) { _mm_free(ptr); }
+    void deallocate(ValueType* ptr, size_t /*n*/) noexcept { _mm_free(ptr); }
 };
 
 template<typename ValueType1, typename ValueType2>
-constexpr bool operator==(const cache_aligned_allocator_t<ValueType1>&, const cache_aligned_allocator_t<ValueType2>&) {
+constexpr bool operator==(const cache_aligned_allocator_t<ValueType1>&,
+                          const cache_aligned_allocator_t<ValueType2>&) noexcept {
     return std::same_as<ValueType1, ValueType2>;
 }
 
 template<typename ValueType1, typename ValueType2>
 constexpr bool operator!=(const cache_aligned_allocator_t<ValueType1>& a,
-                          const cache_aligned_allocator_t<ValueType2>& b) {
+                          const cache_aligned_allocator_t<ValueType2>& b) noexcept {
     return !(a == b);
 }
 } // namespace dim::memory
