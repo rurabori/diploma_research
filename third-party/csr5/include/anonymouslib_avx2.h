08d897e4dd4ff5289c333c461ea972d276b4615b
Boris Rúra <boris.rura@gmail.com>
2021-07-02 12:58:48 +0200

    Start fixing warnings in CSR5 impl + modernize.

--- third-party/csr5/include/anonymouslib_avx2.h
+++ third-party/csr5/include/anonymouslib_avx2.h
@@ -1,23 +1,28 @@
-#ifndef ANONYMOUSLIB_AVX2_H
-#define ANONYMOUSLIB_AVX2_H
+#ifndef THIRD_PARTY_CSR5_INCLUDE_ANONYMOUSLIB_AVX2
+#define THIRD_PARTY_CSR5_INCLUDE_ANONYMOUSLIB_AVX2
 
 #include "detail/avx2/utils_avx2.h"
+#include "detail/common.h"
 #include "detail/utils.h"
 
 #include "detail/avx2/common_avx2.h"
 #include "detail/avx2/csr5_spmv_avx2.h"
 #include "detail/avx2/format_avx2.h"
 
+#include <dim/memory/aligned_allocator.h>
+#include <utility>
+#include <vector>
+
 namespace csr5::avx2 {
+
+template<typename Ty>
+using cache_aligned_vector = std::vector<Ty, dim::memory::cache_aligned_allocator_t<Ty>>;
 
 template<class ANONYMOUSLIB_IT, class ANONYMOUSLIB_UIT, class ANONYMOUSLIB_VT>
 class anonymouslibHandle
 {
 public:
-    anonymouslibHandle(ANONYMOUSLIB_IT m, ANONYMOUSLIB_IT n) {
-        _m = m;
-        _n = n;
-    }
+    anonymouslibHandle(ANONYMOUSLIB_IT m, ANONYMOUSLIB_IT n) : _m{m}, _n{n} {}
     int warmup();
     int inputCSR(ANONYMOUSLIB_IT nnz, ANONYMOUSLIB_IT* csr_row_pointer, ANONYMOUSLIB_IT* csr_column_index,
                  ANONYMOUSLIB_VT* csr_value);
@@ -30,7 +35,7 @@ public:
 
 private:
     int computeSigma();
-    int _format;
+    int _format{};
     ANONYMOUSLIB_IT _m;
     ANONYMOUSLIB_IT _n;
     ANONYMOUSLIB_IT _nnz;
@@ -39,20 +44,20 @@ private:
     ANONYMOUSLIB_IT* _csr_column_index;
     ANONYMOUSLIB_VT* _csr_value;
 
-    int _csr5_sigma;
-    int _bit_y_offset;
-    int _bit_scansum_offset;
-    int _num_packet;
+    int _csr5_sigma{};
+    int _bit_y_offset{};
+    int _bit_scansum_offset{};
+    int _num_packet{};
     ANONYMOUSLIB_IT _tail_partition_start;
 
     ANONYMOUSLIB_IT _p;
-    ANONYMOUSLIB_UIT* _csr5_partition_pointer;
-    ANONYMOUSLIB_UIT* _csr5_partition_descriptor;
+    cache_aligned_vector<ANONYMOUSLIB_UIT> _csr5_partition_pointer;
+    cache_aligned_vector<ANONYMOUSLIB_UIT> _csr5_partition_descriptor;
 
     ANONYMOUSLIB_IT _num_offsets;
-    ANONYMOUSLIB_IT* _csr5_partition_descriptor_offset_pointer;
-    ANONYMOUSLIB_IT* _csr5_partition_descriptor_offset;
-    ANONYMOUSLIB_VT* _temp_calibrator;
+    cache_aligned_vector<ANONYMOUSLIB_IT> _csr5_partition_descriptor_offset_pointer;
+    cache_aligned_vector<ANONYMOUSLIB_IT> _csr5_partition_descriptor_offset;
+    cache_aligned_vector<ANONYMOUSLIB_VT> _temp_calibrator;
 
     ANONYMOUSLIB_VT* _x;
 };
@@ -68,9 +73,7 @@ int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::inpu
                                                                                      ANONYMOUSLIB_IT* csr_column_index,
                                                                                      ANONYMOUSLIB_VT* csr_value) {
     _format = ANONYMOUSLIB_FORMAT_CSR;
-
     _nnz = nnz;
-
     _csr_row_pointer = csr_row_pointer;
     _csr_column_index = csr_column_index;
     _csr_value = csr_value;
@@ -80,20 +83,22 @@ int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::inpu
 
 template<class ANONYMOUSLIB_IT, class ANONYMOUSLIB_UIT, class ANONYMOUSLIB_VT>
 int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::asCSR() {
+    if (_format == ANONYMOUSLIB_FORMAT_CSR)
+        return ANONYMOUSLIB_SUCCESS;
+
     int err = ANONYMOUSLIB_SUCCESS;
-
-    if (_format == ANONYMOUSLIB_FORMAT_CSR) return err;
-
     if (_format == ANONYMOUSLIB_FORMAT_CSR5) {
         // convert csr5 data to csr data
-        err = aosoa_transpose(_csr5_sigma, _nnz, _csr5_partition_pointer, _csr_column_index, _csr_value, false);
+        err = aosoa_transpose(_csr5_sigma, _nnz, _csr5_partition_pointer.data(), _csr_column_index, _csr_value, false);
 
         // free the two newly added CSR5 arrays
-        _mm_free(_csr5_partition_pointer);
-        _mm_free(_csr5_partition_descriptor);
-        _mm_free(_temp_calibrator);
-        _mm_free(_csr5_partition_descriptor_offset_pointer);
-        if (_num_offsets) _mm_free(_csr5_partition_descriptor_offset);
+        std::exchange(_csr5_partition_pointer, {});
+        std::exchange(_csr5_partition_descriptor, {});
+        std::exchange(_temp_calibrator, {});
+        std::exchange(_csr5_partition_descriptor_offset_pointer, {});
+
+        if (_num_offsets)
+            std::exchange(_csr5_partition_descriptor_offset, {});
 
         _format = ANONYMOUSLIB_FORMAT_CSR;
     }
@@ -103,10 +108,10 @@ int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::asCS
 
 template<class ANONYMOUSLIB_IT, class ANONYMOUSLIB_UIT, class ANONYMOUSLIB_VT>
 int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::asCSR5() {
+    if (_format == ANONYMOUSLIB_FORMAT_CSR5)
+        return ANONYMOUSLIB_SUCCESS;
+
     int err = ANONYMOUSLIB_SUCCESS;
-
-    if (_format == ANONYMOUSLIB_FORMAT_CSR5) return err;
-
     if (_format == ANONYMOUSLIB_FORMAT_CSR) {
         double malloc_time = 0, tile_ptr_time = 0, tile_desc_time = 0, transpose_time = 0;
         anonymouslib_timer malloc_timer, tile_ptr_timer, tile_desc_timer, transpose_timer;
@@ -141,31 +146,27 @@ int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::asCS
         _p = ceil((double)_nnz / (double)(ANONYMOUSLIB_CSR5_OMEGA * _csr5_sigma));
         cout << "#partition = " << _p << endl;
 
+        int num_thread = omp_get_max_threads();
+
         malloc_timer.start();
         // malloc the newly added arrays for CSR5
-        _csr5_partition_pointer
-          = (ANONYMOUSLIB_UIT*)_mm_malloc((_p + 1) * sizeof(ANONYMOUSLIB_UIT), ANONYMOUSLIB_X86_CACHELINE);
+        _csr5_partition_pointer.resize(_p + 1);
+        _csr5_partition_descriptor.resize(_p * ANONYMOUSLIB_CSR5_OMEGA * _num_packet);
 
-        _csr5_partition_descriptor = (ANONYMOUSLIB_UIT*)_mm_malloc(
-          _p * ANONYMOUSLIB_CSR5_OMEGA * _num_packet * sizeof(ANONYMOUSLIB_UIT), ANONYMOUSLIB_X86_CACHELINE);
-        memset(_csr5_partition_descriptor, 0, _p * ANONYMOUSLIB_CSR5_OMEGA * _num_packet * sizeof(ANONYMOUSLIB_UIT));
+        _temp_calibrator.resize(num_thread * dim::memory::hardware_destructive_interference_size
+                                / sizeof(ANONYMOUSLIB_VT));
 
-        int num_thread = omp_get_max_threads();
-        _temp_calibrator
-          = (ANONYMOUSLIB_VT*)_mm_malloc(num_thread * ANONYMOUSLIB_X86_CACHELINE, ANONYMOUSLIB_X86_CACHELINE);
-        memset(_temp_calibrator, 0, num_thread * ANONYMOUSLIB_X86_CACHELINE);
+        _csr5_partition_descriptor_offset_pointer.resize(_p + 1);
 
-        _csr5_partition_descriptor_offset_pointer
-          = (ANONYMOUSLIB_IT*)_mm_malloc((_p + 1) * sizeof(ANONYMOUSLIB_IT), ANONYMOUSLIB_X86_CACHELINE);
-        memset(_csr5_partition_descriptor_offset_pointer, 0, (_p + 1) * sizeof(ANONYMOUSLIB_IT));
         malloc_time += malloc_timer.stop();
 
         // convert csr data to csr5 data (3 steps)
         // step 1. generate partition pointer
         tile_ptr_timer.start();
-        err = generate_partition_pointer<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT>(_csr5_sigma, _p, _m, _nnz,
-                                                                            _csr5_partition_pointer, _csr_row_pointer);
-        if (err != ANONYMOUSLIB_SUCCESS) return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
+        err = generate_partition_pointer<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT>(
+          _csr5_sigma, _p, _m, _nnz, _csr5_partition_pointer.data(), _csr_row_pointer);
+        if (err != ANONYMOUSLIB_SUCCESS)
+            return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
         tile_ptr_time += tile_ptr_timer.stop();
 
         _tail_partition_start = (_csr5_partition_pointer[_p - 1] << 1) >> 1;
@@ -176,35 +177,36 @@ int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::asCS
         _num_offsets = 0;
         err = generate_partition_descriptor<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT>(
           _csr5_sigma, _p, _m, _bit_y_offset, _bit_scansum_offset, _num_packet, _csr_row_pointer,
-          _csr5_partition_pointer, _csr5_partition_descriptor, _csr5_partition_descriptor_offset_pointer,
-          &_num_offsets);
-        if (err != ANONYMOUSLIB_SUCCESS) return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
+          _csr5_partition_pointer.data(), _csr5_partition_descriptor.data(),
+          _csr5_partition_descriptor_offset_pointer.data(), &_num_offsets);
+        if (err != ANONYMOUSLIB_SUCCESS)
+            return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
         tile_desc_time += tile_desc_timer.stop();
 
         if (_num_offsets) {
             // cout << "has empty rows, _num_offsets = " << _num_offsets << endl;
             malloc_timer.start();
-            _csr5_partition_descriptor_offset
-              = (ANONYMOUSLIB_IT*)_mm_malloc(_num_offsets * sizeof(ANONYMOUSLIB_IT), ANONYMOUSLIB_X86_CACHELINE);
-            // memset(_csr5_partition_descriptor_offset, 0, _num_offsets * sizeof(ANONYMOUSLIB_IT));
+            _csr5_partition_descriptor_offset.resize(_num_offsets);
             malloc_time += malloc_timer.stop();
 
             tile_desc_timer.start();
             err = generate_partition_descriptor_offset<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT>(
               _csr5_sigma, _p, _bit_y_offset, _bit_scansum_offset, _num_packet, _csr_row_pointer,
-              _csr5_partition_pointer, _csr5_partition_descriptor, _csr5_partition_descriptor_offset_pointer,
-              _csr5_partition_descriptor_offset);
+              _csr5_partition_pointer.data(), _csr5_partition_descriptor.data(),
+              _csr5_partition_descriptor_offset_pointer.data(), _csr5_partition_descriptor_offset.data());
 
             // for (int i = 0; i < _num_offsets; i++)
             //    cout << "_csr5_partition_descriptor_offset = " << _csr5_partition_descriptor_offset[i] << endl;
-            if (err != ANONYMOUSLIB_SUCCESS) return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
+            if (err != ANONYMOUSLIB_SUCCESS)
+                return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
             tile_desc_time += tile_desc_timer.stop();
         }
 
         // step 3. transpose column_index and value arrays
         transpose_timer.start();
-        err = aosoa_transpose(_csr5_sigma, _nnz, _csr5_partition_pointer, _csr_column_index, _csr_value, true);
-        if (err != ANONYMOUSLIB_SUCCESS) return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
+        err = aosoa_transpose(_csr5_sigma, _nnz, _csr5_partition_pointer.data(), _csr_column_index, _csr_value, true);
+        if (err != ANONYMOUSLIB_SUCCESS)
+            return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
         transpose_time += transpose_timer.stop();
 
         printf("CSR->CSR5 malloc time = %f ms\n", malloc_time);
@@ -239,8 +241,9 @@ int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::spmv
     if (_format == ANONYMOUSLIB_FORMAT_CSR5) {
         csr5_spmv<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>(
           _csr5_sigma, _p, _m, _bit_y_offset, _bit_scansum_offset, _num_packet, _csr_row_pointer, _csr_column_index,
-          _csr_value, _csr5_partition_pointer, _csr5_partition_descriptor, _csr5_partition_descriptor_offset_pointer,
-          _csr5_partition_descriptor_offset, _temp_calibrator, _tail_partition_start, alpha, _x, y);
+          _csr_value, _csr5_partition_pointer.data(), _csr5_partition_descriptor.data(),
+          _csr5_partition_descriptor_offset_pointer.data(), _csr5_partition_descriptor_offset.data(), _temp_calibrator.data(),
+          _tail_partition_start, alpha, _x, y);
     }
 
     return err;
@@ -263,4 +266,4 @@ int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::comp
 
 } // namespace csr5::avx2
 
-#endif // ANONYMOUSLIB_AVX2_H
+#endif /* THIRD_PARTY_CSR5_INCLUDE_ANONYMOUSLIB_AVX2 */
