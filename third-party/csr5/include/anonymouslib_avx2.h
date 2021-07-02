7701874f9a5cc046e1ca38d7e0c6bdf48adb5c7a
Boris Rúra <boris.rura@gmail.com>
2021-07-02 21:14:41 +0200

    More modernizations/warning fixes.

--- third-party/csr5/include/anonymouslib_avx2.h
+++ third-party/csr5/include/anonymouslib_avx2.h
@@ -13,6 +13,8 @@
 #include <vector>
 
 #include <dim/memory/aligned_allocator.h>
+#include <fmt/chrono.h>
+#include <fmt/format.h>
 
 namespace csr5::avx2 {
 
@@ -112,119 +114,67 @@ int anonymouslibHandle<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT, ANONYMOUSLIB_VT>::asCS
     if (_format == ANONYMOUSLIB_FORMAT_CSR5)
         return ANONYMOUSLIB_SUCCESS;
 
-    int err = ANONYMOUSLIB_SUCCESS;
-    if (_format == ANONYMOUSLIB_FORMAT_CSR) {
-        double malloc_time = 0;
-        double tile_ptr_time = 0;
-        double tile_desc_time = 0;
-        double transpose_time = 0;
-        anonymouslib_timer malloc_timer{};
-        anonymouslib_timer tile_ptr_timer{};
-        anonymouslib_timer tile_desc_timer{};
-        anonymouslib_timer transpose_timer{};
+    if (_format != ANONYMOUSLIB_FORMAT_CSR)
+        return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
 
-        // compute sigma
-        _csr5_sigma = computeSigma();
-        cout << "omega = " << ANONYMOUSLIB_CSR5_OMEGA << ", sigma = " << _csr5_sigma << ". ";
+    // compute sigma
+    _csr5_sigma = computeSigma();
 
-        // compute how many bits required for `y_offset' and `carry_offset'
-        int base = 2;
-        _bit_y_offset = 1;
-        while (base < ANONYMOUSLIB_CSR5_OMEGA * _csr5_sigma) {
-            base *= 2;
-            _bit_y_offset++;
-        }
+    _bit_y_offset = static_cast<int>(get_needed_storage(ANONYMOUSLIB_CSR5_OMEGA * _csr5_sigma));
+    _bit_scansum_offset = static_cast<int>(get_needed_storage(ANONYMOUSLIB_CSR5_OMEGA));
 
-        base = 2;
-        _bit_scansum_offset = 1;
-        while (base < ANONYMOUSLIB_CSR5_OMEGA) {
-            base *= 2;
-            _bit_scansum_offset++;
-        }
+    if (_bit_y_offset + _bit_scansum_offset
+        > static_cast<int>(sizeof(ANONYMOUSLIB_UIT) * 8 - 1)) // the 1st bit of bit-flag should be in the first packet
+        return ANONYMOUSLIB_UNSUPPORTED_CSR5_OMEGA;
 
-        if (_bit_y_offset + _bit_scansum_offset
-            > sizeof(ANONYMOUSLIB_UIT) * 8 - 1) // the 1st bit of bit-flag should be in the first packet
-            return ANONYMOUSLIB_UNSUPPORTED_CSR5_OMEGA;
+    _num_packet = static_cast<int>(std::ceil(static_cast<double>(_bit_y_offset + _bit_scansum_offset + _csr5_sigma)
+                                             / static_cast<double>(sizeof(ANONYMOUSLIB_UIT) * 8)));
 
-        int bit_all = _bit_y_offset + _bit_scansum_offset + _csr5_sigma;
-        _num_packet = ceil(static_cast<double>(bit_all) / (double)(sizeof(ANONYMOUSLIB_UIT) * 8));
+    // calculate the number of partitions
+    _p = static_cast<ANONYMOUSLIB_IT>(
+      std::ceil(static_cast<double>(_nnz) / static_cast<double>(ANONYMOUSLIB_CSR5_OMEGA * _csr5_sigma)));
 
-        // calculate the number of partitions
-        _p = ceil((double)_nnz / (double)(ANONYMOUSLIB_CSR5_OMEGA * _csr5_sigma));
-        cout << "#partition = " << _p << endl;
+    // malloc the newly added arrays for CSR5
+    _csr5_partition_pointer.resize(_p + 1);
+    _csr5_partition_descriptor.resize(_p * ANONYMOUSLIB_CSR5_OMEGA * _num_packet);
 
-        int num_thread = omp_get_max_threads();
+    _temp_calibrator.resize(omp_get_max_threads() * dim::memory::hardware_destructive_interference_size
+                            / sizeof(ANONYMOUSLIB_VT));
 
-        malloc_timer.start();
-        // malloc the newly added arrays for CSR5
-        _csr5_partition_pointer.resize(_p + 1);
-        _csr5_partition_descriptor.resize(_p * ANONYMOUSLIB_CSR5_OMEGA * _num_packet);
+    _csr5_partition_descriptor_offset_pointer.resize(_p + 1);
 
-        _temp_calibrator.resize(num_thread * dim::memory::hardware_destructive_interference_size
-                                / sizeof(ANONYMOUSLIB_VT));
+    if (generate_partition_pointer<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT>(_csr5_sigma, _p, _m, _nnz,
+                                                                      _csr5_partition_pointer.data(), _csr_row_pointer)
+        != ANONYMOUSLIB_SUCCESS)
+        return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
 
-        _csr5_partition_descriptor_offset_pointer.resize(_p + 1);
+    _tail_partition_start = (_csr5_partition_pointer[_p - 1] << 1) >> 1;
 
-        malloc_time += malloc_timer.stop();
-
-        // convert csr data to csr5 data (3 steps)
-        // step 1. generate partition pointer
-        tile_ptr_timer.start();
-        err = generate_partition_pointer<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT>(
-          _csr5_sigma, _p, _m, _nnz, _csr5_partition_pointer.data(), _csr_row_pointer);
-        if (err != ANONYMOUSLIB_SUCCESS)
-            return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
-        tile_ptr_time += tile_ptr_timer.stop();
-
-        _tail_partition_start = (_csr5_partition_pointer[_p - 1] << 1) >> 1;
-        // cout << "_tail_partition_start = " << _tail_partition_start << endl;
-
-        // step 2. generate partition descriptor
-        tile_desc_timer.start();
-        _num_offsets = 0;
-        err = generate_partition_descriptor<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT>(
+    // step 2. generate partition descriptor
+    if (generate_partition_descriptor<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT>(
           _csr5_sigma, _p, _m, _bit_y_offset, _bit_scansum_offset, _num_packet, _csr_row_pointer,
           _csr5_partition_pointer.data(), _csr5_partition_descriptor.data(),
-          _csr5_partition_descriptor_offset_pointer.data(), &_num_offsets);
-        if (err != ANONYMOUSLIB_SUCCESS)
-            return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
-        tile_desc_time += tile_desc_timer.stop();
+          _csr5_partition_descriptor_offset_pointer.data(), &_num_offsets)
+        != ANONYMOUSLIB_SUCCESS)
+        return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
 
-        if (_num_offsets) {
-            // cout << "has empty rows, _num_offsets = " << _num_offsets << endl;
-            malloc_timer.start();
-            _csr5_partition_descriptor_offset.resize(_num_offsets);
-            malloc_time += malloc_timer.stop();
-
-            tile_desc_timer.start();
-            err = generate_partition_descriptor_offset<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT>(
+    if (_num_offsets) {
+        _csr5_partition_descriptor_offset.resize(_num_offsets);
+        if (generate_partition_descriptor_offset<ANONYMOUSLIB_IT, ANONYMOUSLIB_UIT>(
               _csr5_sigma, _p, _bit_y_offset, _bit_scansum_offset, _num_packet, _csr_row_pointer,
               _csr5_partition_pointer.data(), _csr5_partition_descriptor.data(),
-              _csr5_partition_descriptor_offset_pointer.data(), _csr5_partition_descriptor_offset.data());
-
-            // for (int i = 0; i < _num_offsets; i++)
-            //    cout << "_csr5_partition_descriptor_offset = " << _csr5_partition_descriptor_offset[i] << endl;
-            if (err != ANONYMOUSLIB_SUCCESS)
-                return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
-            tile_desc_time += tile_desc_timer.stop();
-        }
-
-        // step 3. transpose column_index and value arrays
-        transpose_timer.start();
-        err = aosoa_transpose(_csr5_sigma, _nnz, _csr5_partition_pointer.data(), _csr_column_index, _csr_value, true);
-        if (err != ANONYMOUSLIB_SUCCESS)
+              _csr5_partition_descriptor_offset_pointer.data(), _csr5_partition_descriptor_offset.data())
+            != ANONYMOUSLIB_SUCCESS)
             return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
-        transpose_time += transpose_timer.stop();
-
-        printf("CSR->CSR5 malloc time = %f ms\n", malloc_time);
-        printf("CSR->CSR5 tile_ptr time = %f ms\n", tile_ptr_time);
-        printf("CSR->CSR5 tile_desc time = %f ms\n", tile_desc_time);
-        printf("CSR->CSR5 transpose time = %f ms\n", transpose_time);
-
-        _format = ANONYMOUSLIB_FORMAT_CSR5;
     }
 
-    return err;
+    if (aosoa_transpose(_csr5_sigma, _nnz, _csr5_partition_pointer.data(), _csr_column_index, _csr_value, true)
+        != ANONYMOUSLIB_SUCCESS)
+        return ANONYMOUSLIB_CSR_TO_CSR5_FAILED;
+
+    _format = ANONYMOUSLIB_FORMAT_CSR5;
+
+    return ANONYMOUSLIB_SUCCESS;
 }
 
 template<class ANONYMOUSLIB_IT, class ANONYMOUSLIB_UIT, class ANONYMOUSLIB_VT>
--- third-party/csr5/include/detail/utils.h
+++ third-party/csr5/include/detail/utils.h
@@ -7,6 +7,9 @@
 #include <sys/time.h>
 #include <sys/types.h>
 
+#include <chrono>
+
+
 namespace csr5::avx2 {
 
 template<typename iT, typename vT>
@@ -41,9 +44,29 @@ void print_tile(T* input, int m, int n) {
 
 template<typename T>
 void print_1darray(T* input, int l) {
-    for (int i = 0; i < l; i++) cout << input[i] << ", ";
+    for (int i = 0; i < l; i++)
+        cout << input[i] << ", ";
     cout << endl;
 }
+
+template<typename Integral>
+constexpr size_t get_needed_storage(Integral value) {
+    size_t retval{1};
+
+    // TODO: this might be doable with bit magic without a loop.
+    for (Integral base = 2; base < value; base *= 2)
+        ++retval;
+
+    return retval;
+}
+
+template<typename Callable>
+auto timed_section(Callable&& callable) {
+    auto begin = std::chrono::steady_clock::now();
+    std::forward<Callable>(callable)();
+    return std::chrono::steady_clock::now() - begin;
+}
+
 } // namespace csr5::avx2
 
 #endif // UTILS_H
