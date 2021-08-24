3400a5f7fb8447e64c61786d7a6ee321eef72b46
Boris Rúra <boris.rura@gmail.com>
2021-08-25 00:35:45 +0200

    Back to 20 we go.

--- cmake/default_settings.cmake
+++ cmake/default_settings.cmake
@@ -10,9 +10,9 @@ if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
                                                "MinSizeRel" "RelWithDebInfo")
 endif()
 
-# default the standard to 17.
+# default the standard to 20.
 set(CMAKE_CXX_STANDARD
-    17
+    20
     CACHE STRING "C++ standard")
 
 # so that we get AVX (TODO: CMakePreset or toolchain should probably set this.)
