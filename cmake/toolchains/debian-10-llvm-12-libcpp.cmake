9e51e33fc28db5743f9b5fdf20f5214811d6fcd5
Boris Rúra <boris.rura@gmail.com>
2021-09-05 22:00:49 +0200

    CI.

--- cmake/toolchains/debian-10-llvm-12-libcpp.cmake
+++ cmake/toolchains/debian-10-llvm-12-libcpp.cmake
@@ -3,5 +3,3 @@ set(CMAKE_C_COMPILER ${tools}/clang-12)
 set(CMAKE_CXX_COMPILER ${tools}/clang++-12)
 
 set(CMAKE_CXX_FLAGS "-stdlib=libc++")
-list(APPEND CMAKE_PREFIX_PATH "/usr/lib/petsc/lib/pkgconfig/")
-set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH ON)
