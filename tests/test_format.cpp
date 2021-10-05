af9ed551b67a6927606f603ca5e0efe39acb41c4
Boris Rúra <boris.rura@gmail.com>
2021-10-05 23:20:48 +0200

    Fix unit tests.

--- tests/test_format.cpp
+++ tests/test_format.cpp
@@ -9,8 +9,8 @@ using dim::io::formattable_bytes;
 
 TEST_CASE("test bytes formatting") {
     REQUIRE_EQ(fmt::format("{}", formattable_bytes{700}), "700B");
-    REQUIRE_EQ(fmt::format("{}", formattable_bytes{1024}), "1kB");
-    REQUIRE_EQ(fmt::format("{}", formattable_bytes{1'048'576}), "1MB");
-    REQUIRE_EQ(fmt::format("{}", formattable_bytes{1'073'741'824}), "1GB");
+    REQUIRE_EQ(fmt::format("{}", formattable_bytes{1024}), "1kiB");
+    REQUIRE_EQ(fmt::format("{}", formattable_bytes{1'048'576}), "1MiB");
+    REQUIRE_EQ(fmt::format("{}", formattable_bytes{1'073'741'824}), "1GiB");
     REQUIRE_EQ(fmt::format("{}/s", formattable_bytes{500.}), "500B/s");
 }
