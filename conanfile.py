c7e0a4029a388460749ada6c438032f46aedc94a
Boris Rúra <boris.rura@gmail.com>
2021-10-26 01:00:13 +0200

    Try forcing shared openmpi.

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,7 @@ class CSPConan(ConanFile):
 
     requires = ['doctest/2.4.6', 'fmt/8.0.1', 'tclap/1.2.4', 'magic_enum/0.7.3',
                 'stx/1.0.1', 'scnlib/0.4', 'hdf5/1.12.0', 'spdlog/1.9.2', 'structopt/0.1.2',
-                'libcurl/7.78.0', 'libarchive/3.5.1']
+                'libcurl/7.78.0', 'libarchive/3.5.1', 'openmpi/4.1.0']
 
     def _configure_cmake(self):
         cmake = CMake(self)
@@ -28,6 +28,11 @@ class CSPConan(ConanFile):
     def configure(self):
         if self.settings.compiler == 'Visual Studio':
             del self.options.fPIC
+
+        # can't have HDF5 C++ bindings in MPI enabled version.
+        self.options['hdf5'].parallel = True
+        self.options['hdf5'].enable_cxx = False 
+        self.options['openmpi'].shared = True
 
     def build(self):
         cmake = self._configure_cmake()
