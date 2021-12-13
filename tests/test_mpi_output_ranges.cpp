e0e9157de19d591e357d0c2151a5f42542df883a
Boris Rúra <boris.rura@gmail.com>
2021-12-13 15:11:47 +0100

    Add unit tests for synchronizers.

---
+++ tests/test_mpi_output_ranges.cpp
@@ -0,0 +1,37 @@
+#include <doctest/doctest.h>
+
+#include <cstddef>
+#include <cstdint>
+#include <span>
+#include <variant>
+#include <vector>
+
+#include <dim/csr5_mpi/output_range.h>
+
+using dim::csr5_mpi::output_range_t;
+
+TEST_CASE("single runs") {
+    std::vector<output_range_t> runs{{0, 1}, {2, 4}, {5, 6}};
+
+    REQUIRE_EQ(output_range_t::syncs_downto(runs, 0), 0);
+    REQUIRE_EQ(output_range_t::syncs_downto(runs, 1), 1);
+    REQUIRE_EQ(output_range_t::syncs_downto(runs, 2), 2);
+}
+
+TEST_CASE("overlapping runs") {
+    std::vector<output_range_t> runs{{0, 1}, {1, 4}, {4, 6}};
+
+    REQUIRE_EQ(output_range_t::syncs_downto(runs, 0), 0);
+    REQUIRE_EQ(output_range_t::syncs_downto(runs, 1), 0);
+    REQUIRE_EQ(output_range_t::syncs_downto(runs, 2), 1);
+}
+
+TEST_CASE("contiguous overlapping runs") {
+    std::vector<output_range_t> runs{{0, 1}, {1, 1}, {1, 1}, {1, 4}, {4, 6}};
+
+    REQUIRE_EQ(output_range_t::syncs_downto(runs, 0), 0);
+    REQUIRE_EQ(output_range_t::syncs_downto(runs, 1), 0);
+    REQUIRE_EQ(output_range_t::syncs_downto(runs, 2), 0);
+    REQUIRE_EQ(output_range_t::syncs_downto(runs, 3), 0);
+    REQUIRE_EQ(output_range_t::syncs_downto(runs, 4), 3);
+}
