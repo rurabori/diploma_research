fc84a659750e70fa351a156e082d4d5a4871d38c
Boris Rúra <boris.rura@gmail.com>
2021-12-13 15:08:42 +0100

    Use csr5_info to load matrix chunks properly.

--- libs/dim/io/h5.cpp
+++ libs/dim/io/h5.cpp
@@ -47,10 +47,10 @@ auto store(group_view_t group, const dim::mat::csr5<double>& csr5, const csr5_st
     write_dataset_2(group, "vals", csr5.vals, props.vals);
     write_dataset_2(group, "col_idx", csr5.col_idx, props.col_idx);
     write_dataset_2(group, "row_ptr", csr5.row_ptr, props.row_ptr);
-    write_dataset_2(group, "tile_ptr", csr5.tile_ptr, props.tile_ptr);
-    write_dataset_2(group, "tile_desc", csr5.tile_desc, props.tile_desc);
-    write_dataset_2(group, "tile_desc_offset_ptr", csr5.tile_desc_offset_ptr, props.tile_desc_offset_ptr);
-    write_dataset_2(group, "tile_desc_offset", csr5.tile_desc_offset, props.tile_desc_offset);
+    write_dataset_2(group, "tile_ptr", csr5.csr5_info.tile_ptr, props.tile_ptr);
+    write_dataset_2(group, "tile_desc", csr5.csr5_info.tile_desc, props.tile_desc);
+    write_dataset_2(group, "tile_desc_offset_ptr", csr5.csr5_info.tile_desc_offset_ptr, props.tile_desc_offset_ptr);
+    write_dataset_2(group, "tile_desc_offset", csr5.csr5_info.tile_desc_offset, props.tile_desc_offset);
 }
 
 auto load_csr5(group_view_t group) -> dim::mat::csr5<double> {
@@ -59,12 +59,12 @@ auto load_csr5(group_view_t group) -> dim::mat::csr5<double> {
     retval_t retval{.vals = group.open_dataset("vals"),
                     .col_idx = group.open_dataset("col_idx"),
                     .row_ptr = group.open_dataset("row_ptr"),
-                    .tile_ptr = group.open_dataset("tile_ptr"),
-                    .tile_desc = group.open_dataset("tile_desc"),
-                    .tile_desc_offset_ptr = group.open_dataset("tile_desc_offset_ptr"),
-                    .tile_desc_offset = group.open_dataset("tile_desc_offset")};
+                    .csr5_info = {.tile_ptr = group.open_dataset("tile_ptr"),
+                                  .tile_desc = group.open_dataset("tile_desc"),
+                                  .tile_desc_offset_ptr = group.open_dataset("tile_desc_offset_ptr"),
+                                  .tile_desc_offset = group.open_dataset("tile_desc_offset")}};
 
-    retval.tile_count = retval.tile_desc.size();
+    retval.csr5_info.tile_count = retval.csr5_info.tile_desc.size();
     retval.dimensions = mat::dimensions_t{.rows = static_cast<uint32_t>(retval.row_ptr.size() - 1),
                                           .cols = group.open_attribute("column_count")};
 
@@ -87,65 +87,86 @@ auto is_hdf5(const std::filesystem::path& path) -> bool {
            && std::equal(std::begin(buffer), std::end(buffer), std::begin(magic));
 }
 
-auto calculate_tile_chunk(dataspace_view_t dataspace, csr5_partial_identifier_t part) {
-    const auto total_tile_count = dataspace.get_dim();
+namespace {
+    struct tile_chunk_t
+    {
+        size_t start;
+        size_t count;
+    };
 
-    const auto part_size
-      = static_cast<hsize_t>(std::ceil(static_cast<double>(total_tile_count) / static_cast<double>(part.total_count)));
+    auto calculate_tile_chunk(dataspace_view_t dataspace, csr5_partial_identifier_t part) -> tile_chunk_t {
+        const auto total_tile_count = dataspace.get_dim();
 
-    const auto first_tile = part_size * part.idx;
-    const auto tile_count = std::min(part_size, total_tile_count - first_tile);
+        const auto part_size = static_cast<hsize_t>(
+          std::ceil(static_cast<double>(total_tile_count) / static_cast<double>(part.total_count)));
 
-    return std::pair{first_tile, tile_count};
-}
+        const auto first_tile = part_size * part.idx;
+        const auto tile_count = std::min(part_size, total_tile_count - first_tile);
 
-auto load_csr5_partial(group_view_t group, csr5_partial_identifier_t part) -> mat::csr5<double> {
+        return {.start = first_tile, .count = tile_count};
+    }
+
+    auto load_csr5_info_partial(group_view_t group, dataset_view_t tiles_dataset, tile_chunk_t chunk)
+      -> csr5_t::csr5_info_t {
+        auto&& tile_desc = tiles_dataset.read_slab<decltype(csr5_t::csr5_info_t::tile_desc)>(chunk.start, chunk.count);
+
+        auto&& tile_ptr = group.open_dataset("tile_ptr")
+                            .read_slab<decltype(csr5_t::csr5_info_t::tile_ptr)>(chunk.start, chunk.count + 1);
+
+        auto&& tile_desc_offset_ptr
+          = group.open_dataset("tile_desc_offset_ptr")
+              .read_slab<decltype(csr5_t::csr5_info_t::tile_desc_offset_ptr)>(chunk.start, chunk.count + 1);
+
+        const auto tile_desc_offset_ptr_start = tile_desc_offset_ptr.front();
+        const auto tile_desc_offset_ptr_end = tile_desc_offset_ptr.back();
+        auto&& tile_desc_offset
+          = group.open_dataset("tile_desc_offset")
+              .read_slab<decltype(csr5_t::csr5_info_t::tile_desc_offset)>(
+                tile_desc_offset_ptr_start, tile_desc_offset_ptr_end - tile_desc_offset_ptr_start);
+
+        return {
+          .tile_count = chunk.count,
+          .tile_ptr = std::move(tile_ptr),
+          .tile_desc = std::move(tile_desc),
+          .tile_desc_offset_ptr = std::move(tile_desc_offset_ptr),
+          .tile_desc_offset = std::move(tile_desc_offset),
+        };
+    }
+} // namespace
+
+auto load_csr5_partial(group_view_t group, csr5_partial_identifier_t part) -> csr5_t {
     constexpr auto tile_size = csr5_t::tile_size();
 
     const auto is_last_part = part.idx == (part.total_count - 1);
 
     const auto tiles_dataset = group.open_dataset("tile_desc");
-    const auto [first_tile, tile_count] = calculate_tile_chunk(tiles_dataset.get_dataspace(), part);
+    const auto chunk = calculate_tile_chunk(tiles_dataset.get_dataspace(), part);
 
-    auto&& tile_desc = tiles_dataset.read_slab<decltype(csr5_t::tile_desc)>(first_tile, tile_count);
-
-    auto&& tile_ptr = group.open_dataset("tile_ptr").read_slab<decltype(csr5_t::tile_ptr)>(first_tile, tile_count + 1);
-
-    auto&& tile_desc_offset_ptr = group.open_dataset("tile_desc_offset_ptr")
-                                    .read_slab<decltype(csr5_t::tile_desc_offset_ptr)>(first_tile, tile_count + 1);
-
-    const auto tile_desc_offset_ptr_start = tile_desc_offset_ptr.front();
-    const auto tile_desc_offset_ptr_end = tile_desc_offset_ptr.back();
-    auto&& tile_desc_offset = group.open_dataset("tile_desc_offset")
-                                .read_slab<decltype(csr5_t::tile_desc_offset)>(
-                                  tile_desc_offset_ptr_start, tile_desc_offset_ptr_end - tile_desc_offset_ptr_start);
+    auto&& csr5_info = load_csr5_info_partial(group, tiles_dataset, chunk);
 
     const auto row_ptr_dataset = group.open_dataset("row_ptr");
     const auto row_ptr_length = row_ptr_dataset.get_dataspace().get_dim();
+
     const auto dimensions
       = mat::dimensions_t{.rows = static_cast<uint32_t>(row_ptr_dataset.get_dataspace().get_dim() - 1),
                           .cols = group.open_attribute("column_count").read<uint32_t>()};
 
-    const auto row_start = tile_ptr.front().idx();
+    const auto row_start = csr5_info.first_row_idx();
     // for last process, we need to load even rows after the last tile to compute the tail sum.
     // +1 because indices are 0 based and the tile_ptr needs to access row_ptr[row_end]
-    const auto row_end = is_last_part ? row_ptr_length : tile_ptr.back().idx() + 1;
+    const auto row_end = is_last_part ? row_ptr_length : (csr5_info.last_row_idx() + 1);
     auto&& row_ptr = group.open_dataset("row_ptr").read_slab<decltype(csr5_t::row_ptr)>(row_start, row_end - row_start);
 
     const auto vals_dataset = group.open_dataset("vals");
-    const auto val_start = first_tile * tile_size;
-    const auto val_end = is_last_part ? vals_dataset.get_dataspace().get_dim() : (val_start + tile_count * tile_size);
+    const auto val_start = chunk.start * tile_size;
+    const auto val_end = is_last_part ? vals_dataset.get_dataspace().get_dim() : (val_start + chunk.count * tile_size);
 
     const auto col_idx_dataset = group.open_dataset("col_idx");
     return csr5_t{.dimensions = dimensions,
                   .vals = vals_dataset.read_slab<decltype(csr5_t::vals)>(val_start, val_end - val_start),
                   .col_idx = col_idx_dataset.read_slab<decltype(csr5_t::col_idx)>(val_start, val_end - val_start),
                   .row_ptr = std::move(row_ptr),
-                  .tile_count = tile_count,
-                  .tile_ptr = std::move(tile_ptr),
-                  .tile_desc = std::move(tile_desc),
-                  .tile_desc_offset_ptr = std::move(tile_desc_offset_ptr),
-                  .tile_desc_offset = std::move(tile_desc_offset),
+                  .csr5_info = std::move(csr5_info),
                   // TODO: these might be more appropriate as arguments to the SpMV call.
                   .val_offset = val_start,
                   .skip_tail = !is_last_part};
