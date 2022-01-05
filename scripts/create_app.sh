911eec6fabf9803e82b1c34a669190fec35b77f6
Boris Rúra <boris.rura@gmail.com>
2022-01-06 00:18:03 +0100

    Remove unused.

--- scripts/create_app.sh
+++
@@ -1,8 +0,0 @@
-name="${1}"
-
-appdir="apps/$name"
-mkdir -p $appdir
-
-echo -e "brr_add_executable($name)\ninstall(TARGETS $name RUNTIME DESTINATION \${CMAKE_INSTALL_BINDIR})\n" >"$appdir/CMakeLists.txt"
-echo -e "int main(int argc, const char* argv[]) { return 0; }\n" >"$appdir/main.cpp"
-echo -e "add_subdirectory($name)" >>"apps/CMakeLists.txt"
